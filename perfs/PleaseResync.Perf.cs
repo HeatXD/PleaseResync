17e07aaa7dbc99ad55c1d729cf2e3a4e85191cd8
JonathanPicques <jonathan.picques@gmail.com>
2022-02-11 00:29:45 +0100

    feat(perf): add session benchmarking

--- perfs/PleaseResync.Perf.cs
+++ perfs/PleaseResync.Perf.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Attributes;
 
@@ -6,7 +7,62 @@ using PleaseResync;
 
 namespace PleaseResyncPerf
 {
-    public class MessageSerialization
+    public class Benchmark_Session
+    {
+        private const uint INPUT_SIZE = 2;
+        private const ushort FRAME_DELAY = 0;
+        private const ushort LOCAL_PORT_1 = 7001;
+        private const ushort LOCAL_PORT_2 = 7002;
+        private const ushort LOCAL_PORT_3 = 7003;
+        private const string LOCAL_ADDRESS = "127.0.0.1";
+
+        [Benchmark]
+        public void SyncSessions()
+        {
+            uint device1 = 0;
+            uint device2 = 1;
+
+            var adapter1 = new UdpSessionAdapter(LOCAL_PORT_1);
+            var adapter2 = new UdpSessionAdapter(LOCAL_PORT_2);
+            var adapters = new UdpSessionAdapter[] { adapter1, adapter2 };
+
+            var session1 = new Peer2PeerSession(INPUT_SIZE, 2, 2, adapter1);
+            var session2 = new Peer2PeerSession(INPUT_SIZE, 2, 2, adapter2);
+            var sessions = new Peer2PeerSession[] { session1, session2 };
+
+            session1.SetLocalDevice(device1, 1, FRAME_DELAY);
+            session1.AddRemoteDevice(device2, 1, UdpSessionAdapter.CreateRemoteConfig(LOCAL_ADDRESS, LOCAL_PORT_2));
+
+            session2.SetLocalDevice(device2, 1, FRAME_DELAY);
+            session2.AddRemoteDevice(device1, 1, UdpSessionAdapter.CreateRemoteConfig(LOCAL_ADDRESS, LOCAL_PORT_1));
+
+            while (!sessions.All(session => session.IsRunning()))
+            {
+                foreach (var session in sessions)
+                {
+                    session.Poll();
+                }
+            }
+
+            for (var i = 0; i < 100000; i++)
+            {
+                session1.AdvanceFrame(new byte[] { 1, 2 });
+                session2.AdvanceFrame(new byte[] { 3, 4 });
+
+                foreach (var session in sessions)
+                {
+                    session.Poll();
+                }
+            }
+
+            foreach (var adapter in adapters)
+            {
+                adapter.Close();
+            }
+        }
+    }
+
+    public class Benchmark_MessageSerialization
     {
         [Benchmark]
         public void CreateGameInputs()
@@ -33,14 +89,19 @@ namespace PleaseResyncPerf
         }
     }
 
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             BenchmarkRunner.Run(new[]
             {
-                BenchmarkConverter.TypeToBenchmarks(typeof(MessageSerialization))
+                BenchmarkConverter.TypeToBenchmarks(typeof(Benchmark_Session)),
+                BenchmarkConverter.TypeToBenchmarks(typeof(Benchmark_MessageSerialization)),
             });
+
+            // Uncomment this and comment the lines above to run a perf test
+            // var benchmarkSession = new Benchmark_Session();
+            // benchmarkSession.SyncSessions();
         }
     }
 }
