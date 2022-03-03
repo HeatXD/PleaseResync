027c587d23f5d0d62bc19a05f0b2f8c3671cde3e
HeatXD <45072324+HeatXD@users.noreply.github.com>
2022-03-03 01:06:53 +0100

    progress

--- src/input/InputQueueV2.cs
+++ src/input/InputQueueV2.cs
@@ -1,19 +1,24 @@
 
+using System;
+using System.Diagnostics;
+
 namespace PleaseResync
 {
     internal class InputQueueV2
     {
-        public int FirstBadEntry { get => _firstBadEntry; }
+        public int FirstBadFrame { get => _firstBadFrame; }
         public uint FrameDelay { get => _frameDelay; set => _frameDelay = value; }
+
         private const uint QUEUE_SIZE = 128;
+
         private uint _head, _tail, _length;
         private uint _inputSize;
         private uint _playerCount;
         private uint _frameDelay;
-        private bool _firstEntry;
-        private int _lastAddedEntry;
-        private int _firstBadEntry;
-        private int _lastRequestedEntry;
+        private bool _firstFrame;
+        private int _lastAddedFrame;
+        private int _firstBadFrame;
+        private int _lastRequestedFrame;
         private GameInput _prediction;
         private GameInput[] _inputs;
 
@@ -24,13 +29,13 @@ namespace PleaseResync
             _length = 0;
 
             _frameDelay = 0;
-            _firstEntry = true;
+            _firstFrame = true;
             _inputSize = inputSize;
             _playerCount = playerCount;
 
-            _firstBadEntry = GameInput.NullFrame;
-            _lastAddedEntry = GameInput.NullFrame;
-            _lastRequestedEntry = GameInput.NullFrame;
+            _firstBadFrame = GameInput.NullFrame;
+            _lastAddedFrame = GameInput.NullFrame;
+            _lastRequestedFrame = GameInput.NullFrame;
 
             _prediction = EmptyInput();
             _inputs = new GameInput[QUEUE_SIZE];
@@ -48,16 +53,73 @@ namespace PleaseResync
 
         public void ResetPrediction()
         {
-            _lastRequestedEntry = GameInput.NullFrame;
+            _lastRequestedFrame = GameInput.NullFrame;
             _prediction.Frame = GameInput.NullFrame;
-            _firstBadEntry = GameInput.NullFrame;
+            _firstBadFrame = GameInput.NullFrame;
         }
 
         public GameInput VerifiedInput(uint frame)
         {
             uint frameOffset = frame % QUEUE_SIZE;
             if (_inputs[frameOffset].Frame == frame) return _inputs[frameOffset];
-            throw new SessionError("No verified inputs for frame: " + frame);
+            throw new SessionError($"no verified inputs for frame: {frame}");
         }
+
+        public void RemoveVerifiedFrames(int frame)
+        {
+            if (_lastRequestedFrame != GameInput.NullFrame)
+                frame = Math.Min(frame, _lastRequestedFrame);
+
+            if (frame >= _lastAddedFrame)
+            {
+                _tail = _head;
+                _length = 0;
+            }
+            else if (!(frame <= _inputs[_tail].Frame))
+            {
+                uint offset = (uint)(frame - _inputs[_tail].Frame);
+                _tail = (_tail + offset) % QUEUE_SIZE;
+                _length -= offset;
+            }
+        }
+
+        public GameInput GetInput(int frame)
+        {
+            Debug.Assert(_firstBadFrame == GameInput.NullFrame);
+
+            _lastRequestedFrame = frame;
+
+            Debug.Assert(frame >= _inputs[_tail].Frame);
+
+            if (_prediction.Frame == GameInput.NullFrame)
+            {
+                uint offset = (uint)(frame - _inputs[_tail].Frame);
+                if (offset < _length)
+                {
+                    offset = (offset + _tail) % QUEUE_SIZE;
+                    Debug.Assert(_inputs[offset].Frame == frame);
+                    return _inputs[offset];
+                }
+            }
+
+            return null;
+        }
+
+        public int AddInput(GameInput input)
+        {
+
+            return 0;
+        }
+
+        private void AddInputByFrame()
+        {
+
+        }
+
+        private int ProgressQueueHead()
+        {
+            return 0;
+        }
+        private uint PreviousFrame(uint offset) => (((offset) == 0) ? (QUEUE_SIZE - 1) : ((offset) - 1));
     }
 }
